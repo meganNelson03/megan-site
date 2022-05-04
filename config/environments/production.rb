b4089636a2c22cab62a43a38aa167ebbdb8db015
Megan :-) <meganannnelson01@gmail.com>
2022-05-04 13:29:38 -0600

    uglifier fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(:harmony => true)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
